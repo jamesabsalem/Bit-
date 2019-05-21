eabeadb3b8e4513abfddf6017bb3e85b5ec51438
James Absalem Lintu <26407837+jamesabsalem@users.noreply.github.com>
2019-05-21 20:05:19 +0600

    first commit

---
+++ Bit++/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Bit__
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfStatement = Convert.ToInt32(Console.ReadLine());
+            int x = 0;
+            for (int i = 0; i < numberOfStatement; i++)
+            {
+                var statement = Console.ReadLine().Replace("X", "");
+                if (statement == "++")
+                {
+                    x++;
+                }
+                else
+                {
+                    x--;
+                }
+                
+
+            }
+            Console.WriteLine(x);
+        }
+    }
+}
